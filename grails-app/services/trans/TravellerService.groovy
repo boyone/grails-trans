85ad06d7e8a27fdbd9baf00508be750d320534ee
boyone <boyone.g@gmail.com>
2013-10-04 16:22:55 +0700

    create addCheckPoint impl

--- grails-app/services/trans/TravellerService.groovy
+++ grails-app/services/trans/TravellerService.groovy
@@ -5,4 +5,10 @@ class TravellerService {
     def serviceMethod() {
 
     }
+
+    void addCheckPoint(Traveller traveller, String checkPointName, String comment) {
+        CheckPoint checkPoint = new CheckPoint(name: checkPointName).save()
+        CheckPointComment checkPointComment = new CheckPointComment(traveller, checkPoint, comment)
+        checkPointComment.save()
+    }
 }
