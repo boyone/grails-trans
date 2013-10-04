e61eaa4cd2d3277cc1e0593397eb61c5ad7e9b50
boyone <boyone.g@gmail.com>
2013-10-04 16:22:15 +0700

    create Domain

---
+++ grails-app/domain/trans/CheckPointComment.groovy
@@ -0,0 +1,11 @@
+package trans
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: boyone
+ * Date: 10/4/13
+ * Time: 4:18 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class CheckPointComment {
+}
---
+++ grails-app/domain/trans/Traveller.groovy
@@ -0,0 +1,11 @@
+package trans
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: boyone
+ * Date: 10/4/13
+ * Time: 4:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class Traveller {
+}
