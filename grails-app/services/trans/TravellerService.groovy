04be14af59ae37fb83f0af1d4268ecd9ad091151
boyone <boyone.g@gmail.com>
2013-10-04 16:38:05 +0700

    remove unused method

--- grails-app/services/trans/TravellerService.groovy
+++ grails-app/services/trans/TravellerService.groovy
@@ -2,10 +2,6 @@ package trans
 
 class TravellerService {
 
-    def serviceMethod() {
-
-    }
-
     void addCheckPoint(Traveller traveller, String checkPointName, String comment) {
         CheckPoint checkPoint = new CheckPoint(name: checkPointName).save()
         new CheckPointComment(traveller: traveller, checkPoint: checkPoint, comment: comment).save()
