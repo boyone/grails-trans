89d8bccf346689266f1a1d763d78f1dfc85fc3cc
boyone <boyone.g@gmail.com>
2013-10-04 16:21:31 +0700

    create testAddCheckPoint

--- test/unit/trans/TravellerServiceTests.groovy
+++ test/unit/trans/TravellerServiceTests.groovy
@@ -9,9 +9,14 @@ import org.junit.*
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
 @TestFor(TravellerService)
+@Mock([Traveller, CheckPoint, CheckPointComment])
 class TravellerServiceTests {
 
-    void testSomething() {
-        fail "Implement me"
+    void testAddCheckPoint() {
+        Traveller traveller = new Traveller(name: "little-tree").save()
+        service.addCheckPoint(traveller.id, "deadsea", "so good")
+
+        assert 1 == CheckPoint.count
+        assert 1 == CheckPointComment.count
     }
 }
