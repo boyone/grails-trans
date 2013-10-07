7b09b26b6c7b0b8f33a71eb48d9c0ca616a33059
boyone <boyone.g@gmail.com>
2013-10-07 10:12:37 +0700

    extract addCheckPoint to
    createCheckPoint and createCheckPointComment then
    add addCheckPointWithTransaction for transaction management
    (withTransaction)

--- grails-app/services/trans/TravellerService.groovy
+++ grails-app/services/trans/TravellerService.groovy
@@ -2,8 +2,23 @@ package trans
 
 class TravellerService {
 
-    void addCheckPoint(Traveller traveller, String checkPointName, String comment) {
-        CheckPoint checkPoint = new CheckPoint(name: checkPointName).save()
-        new CheckPointComment(traveller: traveller, checkPoint: checkPoint, comment: comment).save()
+    def addCheckPointWithTransaction(Traveller traveller, String checkPointName, String comment) {
+        CheckPoint.withTransaction {
+            CheckPoint checkPoint = createCheckPoint(checkPointName)
+            createCheckPointComment(traveller, checkPoint, comment)
+        }
+    }
+
+    def addCheckPoint(Traveller traveller, String checkPointName, String comment) {
+        CheckPoint checkPoint = createCheckPoint(checkPointName)
+        createCheckPointComment(traveller, checkPoint, comment)
+    }
+
+    def createCheckPoint(String checkPointName) {
+        new CheckPoint(name: checkPointName).save(failOnError:true)
+    }
+
+    def createCheckPointComment(Traveller traveller, CheckPoint checkPoint, String comment) {
+        new CheckPointComment(traveller: traveller, checkPoint: checkPoint, comment: comment).save(failOnError: true)
     }
 }
